56797b438e7d3c4ca3acf395e7ca4aa79606cca3
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-06-29 09:39:26 +0300

    morgan and basic server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // this is it
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+  res.send('Express veikia normaliai');
+});
+
+app.listen('3000', console.log('server is running port 3000'));
