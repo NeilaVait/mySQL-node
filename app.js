e6ed14354cb232c96e645502d94db1bbc9191cec
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-06-29 10:05:40 +0300

    create db, use db

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 // this is it
 const express = require('express');
 const morgan = require('morgan');
+const mysql = require('mysql');
+
+// set up connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodesql',
+});
+
+// connect
+db.connect((err) => {
+  if (err) throw err;
+  console.log('connected to database');
+});
 
 const app = express();
 
+// middleware
 app.use(morgan('common'));
 
 app.get('/', (req, res) => {
   res.send('Express veikia normaliai');
+});
+
+// create database
+app.get('/createdb', (req, res) => {
+  const sql = 'CREATE DATABASE nodesql';
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    // no errors
+    console.log(result);
+    res.send('nodesql duomenu baze sukurta');
+  });
 });
 
 app.listen('3000', console.log('server is running port 3000'));
